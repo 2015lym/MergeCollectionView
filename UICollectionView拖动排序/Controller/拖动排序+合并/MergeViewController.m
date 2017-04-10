8c0148948ece7ae0e3458ff78abf24bb4780604a
gaoyang <gaoy_sy_risenb@163.com>
2017-04-10 15:36:37 +0800

    commit gy - 完成合并功能

--- UICollectionView拖动排序/Controller/拖动排序+合并/MergeViewController.m
+++ UICollectionView拖动排序/Controller/拖动排序+合并/MergeViewController.m
@@ -13,7 +13,7 @@
 #define SCREENHEIGHT [UIScreen mainScreen].bounds.size.height
 #define WIDTH_5S_SCALE 320.0 * [UIScreen mainScreen].bounds.size.width
 
-#define ITEM_NUMBER 50
+#define ITEM_NUMBER 10
 
 static NSString * const kImage = @"kImage";             //logo图片
 static NSString * const kTitle = @"kTitle";             //图片标题
@@ -23,11 +23,11 @@ typedef NS_ENUM(NSInteger, kMoveType){
     kMoveTypeMerge
 };
 @interface MergeViewController ()<UICollectionViewDelegate, UICollectionViewDataSource,UITextFieldDelegate>
-@property (nonatomic, strong) NSMutableArray *dataArray;
-@property (nonatomic, strong) NSMutableArray<NSArray *> *containerArray;
+@property (nonatomic, strong) NSMutableArray *dataArray;//collectionView数据源数组
+@property (nonatomic, strong) NSMutableArray<NSArray *> *containerArray;//记录包含合并的数组
 @property (nonatomic, strong) UICollectionView * collectionView;
 @property (nonatomic, strong) UICollectionView * containerCollectionView;
-@property (nonatomic, assign) kMoveType moveType;
+@property (nonatomic, assign) kMoveType moveType;//移动方式，移动or合并
 @end
 
 @implementation MergeViewController
@@ -44,12 +44,10 @@ typedef NS_ENUM(NSInteger, kMoveType){
     _moveType = kMoveTypeNone;
     
     _dataArray = [NSMutableArray array];
-    //产生随机颜色的方块
+    //添加数据源
     for (int i = 1; i <= ITEM_NUMBER; i++) {
         NSString *str = [NSString stringWithFormat:@"%d", i];
-//        [_dataArray addObject:str];
         UIImage *image = [UIImage imageNamed:@"proper_logo"];
-//        [self.imageArray addObject:image];
         NSDictionary *dic = @{kImage:image,kTitle:str};
         [_dataArray addObject:dic];
         [self.containerArray addObject:@[dic]];
@@ -91,8 +89,8 @@ typedef NS_ENUM(NSInteger, kMoveType){
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     ymCollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ymCollectionViewCell" forIndexPath:indexPath];
-    cell.contentLabel.text = [NSString stringWithFormat:@"请假审批%@",self.dataArray[indexPath.row][kTitle]];
-    cell.imageView.image = self.dataArray[indexPath.row][kImage];
+    cell.contentLabel.text = [NSString stringWithFormat:@"请假审批%@",self.dataArray[indexPath.item][kTitle]];
+    cell.imageView.image = self.dataArray[indexPath.item][kImage];
     return cell;
 }
 
@@ -154,50 +152,56 @@ static NSIndexPath *startIndexPath;   //起始路径
                 CGFloat space = sqrtf(pow(snapedView.center.x - cell.center.x, 2) + powf(snapedView.center.y - cell.center.y, 2));
                 NSLog(@"%f",space);
                 //如果相交一半就移动
-                if (space <= 10.0) {
-                    currentIndexPath = [self.collectionView indexPathForCell:cell];
-                    //                    [self.collectionView moveItemAtIndexPath:oldIndexPath toIndexPath:currentIndexPath];
-                    if (self.containerArray[startIndexPath.row].count!=1) {
-                        return;
-                    }
-                    _moveType = kMoveTypeMerge;
-                    oldIndexPath = currentIndexPath;
-                    break;
-                }
                 if(space <= snapedView.bounds.size.width*3 / 4){
                     currentIndexPath = [self.collectionView indexPathForCell:cell];
                     //移动 会调用willMoveToIndexPath方法更新数据源
                     _moveType = kMoveTypeExchange;
-                    //设置移动后的起始indexPath
+                    //更改移动后的起始indexPath，用于后面获取隐藏的cell,是移动后的位置
                     oldIndexPath = currentIndexPath;
-                    break;
+                }
+                //如果中心距离小于10就合并
+                if (space <= 10.0) {
+                    if (self.containerArray[startIndexPath.row].count==1) {
+                        currentIndexPath = [self.collectionView indexPathForCell:cell];
+                        _moveType = kMoveTypeMerge;
+                        //更改移动后的起始indexPath，用于后面获取隐藏的cell,是移动前的位置
+                        oldIndexPath = startIndexPath;
+                    }else{
+                        
+                    }
                 }
             }
             if (_moveType == kMoveTypeExchange) {
                 [self.collectionView moveItemAtIndexPath:startIndexPath toIndexPath:currentIndexPath];
                 //移除数据插入到新的位置
-                id obj = [_dataArray objectAtIndex:startIndexPath.row];
+                id obj = [_dataArray objectAtIndex:startIndexPath.item];
                 [_dataArray removeObject:[_dataArray objectAtIndex:startIndexPath.item]];
                 [_dataArray insertObject:obj
                                  atIndex:currentIndexPath.item];
+                id containerObj = [self.containerArray objectAtIndex:startIndexPath.item];
+                [self.containerArray removeObject:[self.containerArray objectAtIndex:startIndexPath.item]];
+                [self.containerArray insertObject:containerObj
+                                 atIndex:currentIndexPath.item];
                 
             }else if (_moveType == kMoveTypeMerge){
+                //设置合并后的新数组
                 NSMutableArray *mergeArray = [[NSMutableArray alloc]init];
                 [self.containerArray[currentIndexPath.row] enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
                     [mergeArray addObject:obj];
                 }];
                 [mergeArray addObject:self.containerArray[startIndexPath.item][0]];
                 [self.containerArray replaceObjectAtIndex:currentIndexPath.item withObject:mergeArray];
-                [self.containerArray removeObjectAtIndex:startIndexPath.item];
                 [_dataArray replaceObjectAtIndex:currentIndexPath.row withObject:@{kTitle:@"合成兽",kImage:[self setMergeImageWithImageArray:self.containerArray[currentIndexPath.item]]}];
                 [_dataArray removeObject:[_dataArray objectAtIndex:startIndexPath.item]];
+                [self.containerArray removeObjectAtIndex:startIndexPath.item];
             }
-            UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:oldIndexPath];
+            UICollectionViewCell *cell = [self.collectionView cellForItemAtIndexPath:oldIndexPath];//原来隐藏的cell
+            UICollectionViewCell *targetCell = [self.collectionView cellForItemAtIndexPath:currentIndexPath];//移动目标cell
             //结束动画过程中停止交互，防止出问题
             self.collectionView.userInteractionEnabled = NO;
             //给截图视图一个动画移动到隐藏cell的新位置
             [UIView animateWithDuration:0.25 animations:^{
-                snapedView.center = cell.center;
+                snapedView.center = targetCell.center;
                 snapedView.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
             } completion:^(BOOL finished) {
                 //移除截图视图、显示隐藏的cell并开启交互
@@ -211,13 +215,16 @@ static NSIndexPath *startIndexPath;   //起始路径
     }
 }
 
+#pragma mark - ---------- 合成新图标 ----------
 - (UIImage *)setMergeImageWithImageArray:(NSArray *)imageArray{
+    //新图标大小
     CGSize size = CGSizeMake(SCREENWIDTH/4-40,SCREENWIDTH/4-40);
     UIGraphicsBeginImageContext(size);
+    //从数组中取图片进行拼接
     [imageArray enumerateObjectsUsingBlock:^(NSDictionary* obj, NSUInteger idx, BOOL * _Nonnull stop) {
         UIImage *image = obj[kImage];
         [image drawInRect:CGRectMake(5/WIDTH_5S_SCALE+15/WIDTH_5S_SCALE*(idx%2), 5/WIDTH_5S_SCALE+15/WIDTH_5S_SCALE*(idx/2), 10/WIDTH_5S_SCALE, 10/WIDTH_5S_SCALE)];
-        if (idx>3) {
+        if (idx>=3) {
             *stop = YES;
         }
     }];
